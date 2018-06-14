72d1f215a39d5d6d9943079ad158456d0702c451
Pawel Orlikoski <orliktcz@gmail.com>
2018-06-14 09:26:14 +0200

    struktury danych

---
+++ src/task1/City.java
@@ -0,0 +1,31 @@
+package task1;
+
+public class City {
+   private String name;
+    private Integer amountOfCitizens;
+    private String state;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmountOfCitizens() {
+        return amountOfCitizens;
+    }
+
+    public void setAmountOfCitizens(Integer amountOfCitizens) {
+        this.amountOfCitizens = amountOfCitizens;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ src/task1/Main.java
@@ -0,0 +1,8 @@
+package task1;
+
+public class Main {
+    public static void main(String[] args) {
+        City c1 = new City();
+        City c2 = new City();
+    }
+}
