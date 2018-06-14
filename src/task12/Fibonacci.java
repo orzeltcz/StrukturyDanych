225192e07f550730c03a3e30629a21aa7ac65ab1
Pawel Orlikoski <orliktcz@gmail.com>
2018-06-14 16:35:56 +0200

    fibonaci update

--- src/task12/Fibonacci.java
+++ src/task12/Fibonacci.java
@@ -2,20 +2,21 @@ package task12;
 
 import com.sun.org.apache.xpath.internal.SourceTree;
 
+import java.math.BigInteger;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Fibonacci {
-private static Map<Integer,Long> countedFib = new LinkedHashMap<>();
-    public long licz (int a) {
+private static Map<Integer,BigInteger> countedFib = new LinkedHashMap<>();
+    public BigInteger licz (int a) {
 
             if (a == 0) {
-                return 0;
+                return BigInteger.valueOf(0);
             } else if (a == 1) {
-                return 1;
+                return BigInteger.valueOf(1);
             } else {
                 if(countedFib.containsKey(a)) return countedFib.get(a);
-              else  return licz(a - 1) + licz(a - 2);
+              else  return (licz(a - 1).add(licz(a - 2)));
             }
     }
         public void print(int count){
@@ -24,7 +25,7 @@ private static Map<Integer,Long> countedFib = new LinkedHashMap<>();
 
     public static void main(String[] args) {
         long before,after;
-        int couter=100;
+        int couter=1000;
         Long sum;
         Fibonacci fib = new Fibonacci();
         for (int i = 0; i <= couter; i++) {
