b4036ff9f09d4e718d36cfb050c0cd35dbb819a9
Pawel Orlikoski <orliktcz@gmail.com>
2018-06-14 09:34:46 +0200

    upadte gitignore

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         City c1 = new City();
         City c2 = new City();
+        int tab[] = new int[10];
+        int emptyTab[] = new int[0];
+        double doubleTab[] = { 23.23, 214.232, 11.34 };
+        int twoDimensionTab[][] = new int[10][10];
     }
 }
